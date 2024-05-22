31665aa4691603f53ba05b965b891a1a09d80478
Sujal Raj <sujalraj611@gmail.com>
2024-05-22 23:27:42 +0530

    login screen added

---
+++ lib/features/authentication/screens/login/login.dart
@@ -0,0 +1,50 @@
+import 'package:e_commerce/common/styles/spacing_styles.dart';
+import 'package:e_commerce/features/authentication/screens/login/widgets/login_form.dart';
+import 'package:e_commerce/features/authentication/screens/login/widgets/login_header.dart';
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/constants/text_strings.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../common/widgets/login_signup/form_divider.dart';
+import '../../../../common/widgets/login_signup/social_buttons.dart';
+import '../../../../utils/constants/sizes.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return  Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              // logo, title, subtitle
+              TLoginHeader(dark: dark),
+              ///form
+              const TLoginForm(),
+              //divider
+              TFormDivider(dividerText: TTexts.orSignInWith.capitalize!,),
+              const SizedBox(height: TSizes.spaceBtwSections,),
+
+              // footer
+              const TSocialButton()
+            ],
+          ),
+          ),
+        ),
+    );
+  }
+}
+
+
+
+
+
