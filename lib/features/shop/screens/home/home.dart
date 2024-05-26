771ab57b7683610b4526cc87d1d8cb9d5a2e0681
Sujal Raj <sujalraj611@gmail.com>
2024-05-26 15:21:46 +0530

    home page header categories added

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,34 +1,70 @@
-import 'package:e_commerce/common/widgets/appbar/appbar.dart';
+import 'dart:ui';
+
 import 'package:e_commerce/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:e_commerce/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
-import 'package:e_commerce/utils/constants/text_strings.dart';
+import 'package:e_commerce/utils/constants/image_strings.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/device/device_utility.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:iconsax/iconsax.dart';
 
-import '../../../../common/widgets/custom_shapes/containers/circular_containers.dart';
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
-import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges.dart';
-import '../../../../common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/image_text_widget/vertical_image_text.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Column(
-                children: [
-                  THomeAppBar()
-                ],
-              )
-            ),
+                child: Column(
+              children: [
+                // Appbar --
+                THomeAppBar(),
+                SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+
+                // SearchBar --
+                TSearchContainer(text: 'Search in Store'),
+                SizedBox(height: TSizes.spaceBtwSections),
+
+                // Categories --
+                Padding(
+                  padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                  child: Column(
+                    children: [
+                      // -- Heading
+                      TSectionHeading(
+                        title: 'Popular Categories',
+                        showActionButton: false,
+                        textColor: Colors.white,
+                      ),
+                      SizedBox(height: TSizes.spaceBtwItems),
+
+                      // -- Categories
+                      THomeCategories()
+                    ],
+                  ),
+                )
+              ],
+            )),
+
+            // body----
+            Padding(
+              padding: const EdgeInsets.all(TSizes.defaultSpace),
+              child: TRoundedImage(),
+            )
           ],
         ),
       ),
@@ -36,7 +72,24 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage({
+    super.key,
+  });
 
 
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(TSizes.md),
+      ),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(TSizes.md),
+          child: const Image(
+            image: AssetImage(TImages.promoBanner1), fit: BoxFit.contain,
+          )),
+    );
+  }
+}
